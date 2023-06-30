4a0b4c3bbb536ae60e20ea8eb014c78ce7dcfa62
kjm541006 <kjm541113@gmail.com>
2023-06-30 12:15:25 +0900

    프론트엔드 백엔드 기본 환경 설정

---
+++ backend/catchJob/src/main/java/com/project/catchJob/CatchJobApplication.java
@@ -0,0 +1,13 @@
+package com.project.catchJob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchJobApplication.class, args);
+	}
+
+}
