a711c54b6f8f094b2cac9b835e7a5ebb9880e9f6
choigukhee <cghee0718@naver.com>
2023-06-30 15:57:06 +0900

    pull

--- frontend/catch-job/src/App.js
+++ frontend/catch-job/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './pages/LoginPage';
+
 
 function App() {
   return (
     <div className="App">
-    <h1>hi</h1>
+      <Login/>
     </div>
   );
 }
---
+++ frontend/catch-job/src/pages/LoginPage.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+const Login = () => {
+  return (
+    <div style={styles.background}>
+   <div style={styles.rectangle}>
+
+   </div>
+  </div>
+  );
+}; 
+
+export default Login;
+
+const styles = {
+  background: {
+    height: '100vh',
+    backgroundColor: '#F9FAFB',
+  },
+  rectangle: {
+    width: '200px',
+    height: '100px',
+    borderRadius: '32px',
+    background: '#FFF',
+    padding: '134, 640, 56, 640',
+  },
+};
