78558d374353ec4adb5d8a3441a846ba113260b8
kjm541006 <kjm541113@gmail.com>
2023-07-06 23:51:20 +0900

    header footer 완성 및 레이아웃 깨짐 해결

--- frontend/catch-job/src/App.js
+++ frontend/catch-job/src/App.js
@@ -1,34 +1,29 @@
 import "./App.css";
+import "./reset.css";
 import Header from "./components/header/Header";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Footer from "./components/footer/Footer";
-import Temp from "./pages/Temp";
-import Login from "./pages/LoginPage";
+import LoginPage from "./pages/LoginPage";
 import HeaderFile from "./pages/HeaderFile";
 import SocialSigninPage from "./pages/SocialSigninPage";
 import BasicSigninPage from "./pages/BasicSigninPage";
-import EditSigninPage from './pages/EditSigninPage';
-// import "./reset.css";
-// import Header from "./components/header/Header";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EditSigninPage from "./pages/EditSigninPage";
+import Portfolio from "./pages/Portfolio";
 
 function App() {
   return (
     <div className="App">
-      <HeaderFile/>
-      <BasicSigninPage/>
-      {/* 리액트 라우터 돔  URL경로 표시용 */}
-      {/* <BrowserRouter>
-        <Routes>
-          <Route></Route>
-        </Routes>
-      </BrowserRouter> */}
-      {/* 헤더 컴포넌트 */}
       <Header />
-      {/* 아래는 푸터 컴포넌트 확인용 임시 컴포넌트 입니다. */}
-      <Temp />
+      <Routes>
+        <Route path="/join" element={<BasicSigninPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        {/* <Route path="/userInfo/:userId" element={<UserInfo />} /> */}
+        <Route path="/" element={<Portfolio />} />
+        {/* <Route path="/recruit" element={<Recruit />} /> */}
+        {/* <Route path="/study/*" element={<StudyRoutes />} /> */}
+        {/* <Route path="/community/*" element={<CommunityRoutes />} /> */}
+      </Routes>
       <Footer />
-      {/* <Header /> */}
     </div>
   );
 }
--- frontend/catch-job/src/pages/Temp.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./Temp.module.css";
-
-const Temp = () => {
-  return <div className={styles.Temp}></div>;
-};
-
-export default Temp;
