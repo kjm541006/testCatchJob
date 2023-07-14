6d2944e4725ebe8021792a1373f0a414df9c0842
uujjjjjnn <uujjjjjnn@gmail.com>
2023-07-14 16:20:47 +0900

    google

---
+++ backend/catchJob/src/main/java/com/project/catchJob/dto/member/GoogleUserInfoDTO.java
@@ -0,0 +1,21 @@
+package com.project.catchJob.dto.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class GoogleUserInfoDTO {
+	
+	// 구글 사용자 정보 담을 DTO
+	
+	private String id;
+	private String email;
+	private Boolean verified_email;
+	private String name;
+	private String given_name;
+	private String picture;
+	private String locale;
+}
