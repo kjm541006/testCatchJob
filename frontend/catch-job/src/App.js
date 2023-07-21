6c300e2c499a4910b0844ea6ead3f8ded6e7c43b
kjm541006 <kjm541113@gmail.com>
2023-07-21 18:09:10 +0900

    작동 가능한 코드

--- frontend/catch-job/src/App.js
+++ frontend/catch-job/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import "./reset.css";
 import Header from "./components/header/Header";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import LoginPage from "./pages/LoginPage";
 import SocialSigninPage from "./pages/SocialSigninPage";
@@ -9,22 +9,33 @@ import BasicSigninPage from "./pages/BasicSigninPage";
 import EditSigninPage from "./pages/EditSigninPage";
 import PortfolioMainPage from "./pages/PortfolioMainPage";
 import CommunityRoutes from "./pages/community/CommunityPage";
-import StudyPage from "./pages/StudyPage";
-import BuildStudyPage from "./pages/BuildStudyPage";
+import StudyPage from "./pages/study&project/StudyPage";
+import BuildStudyPage from "./pages/study&project/BuildStudyPage";
 // import GoogleLoginButton from "./components/GoogleLoginButton";
 // import { GoogleOAuthProvider } from "@react-oauth/google";
 // import GoogleLoginButton_long from "./components/GoogleLoginButton_long";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import MyPage from "./pages/MyPage";
-import { setCredentials } from "./redux/login";
+import { selectUserLoggedOut, setCredentials, setTokenFromLocalStorage, userLoggedOut } from "./redux/login";
+import { logOut, selectEmail, selectLoggedIn, selectName } from "./redux/login";
+import LoginRoute from "./components/LoginRoute";
+import { useEffect } from "react";
+import LoginAlertPage from "./pages/LoginAlertPage";
 
 function App() {
   const dispatch = useDispatch();
-
+  const name = localStorage.getItem("name");
+  const email = localStorage.getItem("email");
   const savedToken = localStorage.getItem("token");
-  if (savedToken) {
-    dispatch(setCredentials({ token: savedToken }));
-  }
+  const isLoggedIn = useSelector(selectLoggedIn);
+  const userLoggedOut = useSelector(selectUserLoggedOut);
+
+  useEffect(() => {
+    console.log(isLoggedIn);
+    if (savedToken) {
+      dispatch(setTokenFromLocalStorage({ name, email, token: savedToken }));
+    }
+  }, [isLoggedIn, savedToken, dispatch, email, name]);
 
   return (
     <div className="App">
@@ -35,13 +46,13 @@ function App() {
           <Route path="/" element={<PortfolioMainPage />} />
           {/* <Route path="/recruit" element={<Recruit />} /> */}
           <Route path="/study" element={<StudyPage />} />
-          <Route path="/study/build/*" element={<BuildStudyPage />} />
+          <Route path="/study/build" element={savedToken != null ? <BuildStudyPage /> : <LoginAlertPage />} />
           {/* <Route path="/community/*" element={<CommunityRoutes />} /> */}
         </Route>
         <Route path="/join" element={<SocialSigninPage />} />
         <Route path="/signin" element={<BasicSigninPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/mypage" element={savedToken != null ? <MyPage /> : <LoginAlertPage />} />
         <Route path="/edit" element={<EditSigninPage />} />
         {/* <Route path="/userInfo/:userId" element={<UserInfo />} /> */}
         <Route path="/" element={<PortfolioMainPage />} />
--- frontend/catch-job/src/pages/PortfolioMainPage.jsx
+++ frontend/catch-job/src/pages/PortfolioMainPage.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../assets/css/PortfolioMain.module.css";
+import { useNavigate } from "react-router-dom";
 
 const PortfolioMainPage = () => {
+  // 아래는 react-router-dom 용 코드입니다 유지시켜주세요.
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/");
+  }, [navigate]);
+
   return <div className="temp">Portfolio</div>;
 };
 
