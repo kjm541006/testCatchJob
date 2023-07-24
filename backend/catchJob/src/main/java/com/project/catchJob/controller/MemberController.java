22da0c28f676e8c004042faf7abd16399e744c41
uujjjjjnn <uujjjjjnn@gmail.com>
2023-07-24 10:04:42 +0900

    시큐리티 삭제 포폴 추가

--- backend/catchJob/src/main/java/com/project/catchJob/controller/MemberController.java
+++ backend/catchJob/src/main/java/com/project/catchJob/controller/MemberController.java
@@ -14,19 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.project.catchJob.domain.member.GoogleOAuth;
 import com.project.catchJob.domain.member.Member;
-import com.project.catchJob.dto.member.GoogleOAuthTokenDTO;
-import com.project.catchJob.dto.member.GoogleUserInfoDTO;
 import com.project.catchJob.dto.member.MemberDTO;
 import com.project.catchJob.security.PasswordEncoder;
 import com.project.catchJob.security.TokenProvider;
 import com.project.catchJob.service.MemberService;
 import com.project.catchJob.service.OAuthService;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RequiredArgsConstructor
 @RestController
 @RequestMapping("/")
 public class MemberController {
@@ -39,38 +35,44 @@ public class MemberController {
 	
 	@Autowired
 	private PasswordEncoder pwdEncoder;
-
+/*	
 	@Autowired
 	private GoogleOAuth googleoauth;
 	
 	@Autowired
 	private OAuthService oAuthService;
-	
+*/	
 
+	
 	// 회원등록
 	@PostMapping("/register")
 	public ResponseEntity<?> registerMember(@RequestBody MemberDTO memberDTO) {
 		try {
-			
+			if(memberDTO == null || memberDTO.getPwd() == null) {
+				throw new RuntimeException("비밀번호 공란");
+			}
 			MemberDTO responseMemberDTO = MemberDTO.builder()
 					.email(memberDTO.getEmail())
 					.pwd(pwdEncoder.encrypt(memberDTO.getEmail(), memberDTO.getPwd()))
+//					.pwd(memberDTO.getPwd())
 					.name(memberDTO.getName())
 					.job(memberDTO.getJob())
 					.hasCareer(memberDTO.getHasCareer())
 					.type("일반")
 					.build();
-			System.out.println("=========" + responseMemberDTO);
 			memberService.createMember(responseMemberDTO);
 			return ResponseEntity.ok().body(responseMemberDTO);
 		} catch (Exception e) {
-			return ResponseEntity.badRequest().body("해당 이메일은 이미 존재합니다. 다른 이메일을 입력해주세요.");
+			// 멤버 정보는 항상 하나이므로 리스트로 만들어야하는 ResponseDTO를 사용하지 않고 그냥 member 리턴
+//			 ResponseDTO responseDTO = ResponseDTO.builder().error(e.getMessage()).build();
+//			 return ResponseEntity.badRequest().body(registerMember(memberDTO));
+			 return ResponseEntity.badRequest().body("해당 이메일은 이미 존재합니다. 다른 이메일을 입력해주세요.");
 		}		
 	}
 	
 	
 	/*
-
+>>>>>>> 226a55db1e3b3cb985f2feb38fec422c29f749d0
 	// 회원등록
 	@PostMapping("/register") 
 	public ResponseEntity<?> registerMember(@RequestBody MemberDTO memberDTO) {
@@ -106,16 +108,15 @@ public class MemberController {
 		}
 	}
 	*/
-
-
 	// 로그인
 	@PostMapping("/login") 
 	public ResponseEntity<?> login(@RequestBody MemberDTO memberDTO) {
 		Member member = memberService.getByCredentials(memberDTO.getEmail(), memberDTO.getPwd(), pwdEncoder);
 		
+		// log.info("{} 로그인 성공", member.toString());
 		System.out.println("==========" + member.toString());
 		if(member != null) {
-
+			
 			// 토큰 생성
 			final String token = tokenProvider.createToken(member);
 			log.info("token 생성 성공", token);
@@ -130,83 +131,39 @@ public class MemberController {
 					.build();
 			
 			return ResponseEntity.ok().body(responseMemberDTO);
-		} else {
+		}
+		else {
 			return ResponseEntity.badRequest().body("로그인 실패");
 		}
 	}
-
 	/*
 	// 구글 로그인
-	@GetMapping("/googlelogin")
+	@GetMapping("/google")
 	public void getGoogleAuthUrl(HttpServletResponse res) throws Exception {
 		res.sendRedirect(googleoauth.getOauthRedirectURL());
 	}
 	
-	//@PostMapping("/api/oauth2/callback/google")
-	@PostMapping("/googlelogin")
-	public ResponseEntity<?> successGoogleLogin(@RequestParam("code") String accessCode) throws Exception {
-		System.out.println("---------1.accessCode------" + accessCode);
-		ResponseEntity<String> tokenEntity = googleoauth.requestAccessToken(accessCode);
-		System.out.println("---------2.tokenEntity------" + tokenEntity);
-		GoogleOAuthTokenDTO googleOAuthTokenDTO = googleoauth.getAccessToken(tokenEntity);
-		System.out.println("---------3.googleOAuthTokenDTO------" + googleOAuthTokenDTO);
-		ResponseEntity<String> requsetUser = googleoauth.requestUserInfo(googleOAuthTokenDTO);
-		System.out.println("---------4.requsetUser------" + requsetUser);
-		GoogleUserInfoDTO getUser = googleoauth.getUserInfo(requsetUser);
-		System.out.println("---------5.getUser------" + getUser);
-		
-		MemberDTO createMember = oAuthService.createGoogleUser(accessCode);
-		System.out.println("---------6.createMember------" + createMember.toString());
-		return ResponseEntity.ok().body(createMember.getToken());
+	@GetMapping("/api/oauth2/callback/google")
+	public ResponseEntity<?> successGoogleLogin(@RequestParam("code") String accessCode) {
+		return googleoauth.requestAccessToken(accessCode);
+	}
+	
+	@GetMapping("/google/login")
+	public ResponseEntity<?> successGoogleLogin2(@RequestParam("code") String accessCode) {
+		return null;
+		// return oAuthService.googleLogin(accessCode);
 	}
 */
-	@PostMapping("/googlelogin")
-	public ResponseEntity<?> successGoogleLogin(
-	  @RequestParam("code") String accessCode,
-	  @RequestParam("grant_type") String grantType
-	) throws Exception {
-	  System.out.println("————1.accessCode———" + accessCode);
-	  System.out.println("————2.grantType———" + grantType);
-	  
-	  ResponseEntity<String> tokenEntity = googleoauth.requestAccessToken(accessCode);
-		System.out.println("---------3.tokenEntity------" + tokenEntity);
-		GoogleOAuthTokenDTO googleOAuthTokenDTO = googleoauth.getAccessToken(tokenEntity);
-		System.out.println("---------4.googleOAuthTokenDTO------" + googleOAuthTokenDTO);
-		ResponseEntity<String> requsetUser = googleoauth.requestUserInfo(googleOAuthTokenDTO);
-		System.out.println("---------5.requsetUser------" + requsetUser);
-		GoogleUserInfoDTO getUser = googleoauth.getUserInfo(requsetUser);
-		System.out.println("---------6.getUser------" + getUser);
-		
-		MemberDTO createMember = oAuthService.createGoogleUser(accessCode);
-		System.out.println("---------7.createMember------" + createMember.toString());
-		return ResponseEntity.ok().body(createMember.getToken());
-	}
-	  
-	
-	
-/*
-
 	// 회원조회
-	@GetMapping("/memberInfo")
+	@PostMapping("/memberInfo")
 	public ResponseEntity<?> memberInfo(@RequestBody MemberDTO memberDTO) {
 		Member member = memberService.getByCredentials(memberDTO.getEmail(), memberDTO.getPwd(), pwdEncoder);
 		
 		if(member != null) {
 			MemberDTO responseMemberDTO = MemberDTO.toMemberDTO(member);
 			return ResponseEntity.ok().body(responseMemberDTO);
-		} else {
-			return ResponseEntity.badRequest().body("회원 조회 실패");
 		}
-	}
-	*/
-	@GetMapping("/memberInfo")
-	public ResponseEntity<?> memberInfo(@RequestParam("email") String email, @RequestParam("pwd") String pwd) {
-	    Member member = memberService.getByCredentials(email, pwd, pwdEncoder);
-	    if (member != null) {
-	        MemberDTO responseMemberDTO = MemberDTO.toMemberDTO(member);
-	        return ResponseEntity.ok().body(responseMemberDTO);
-	    }
-	    return ResponseEntity.badRequest().body("회원 조회 실패");
+		return ResponseEntity.badRequest().body("회원 조회 실패");
 	}
 	
 	// 회원수정
@@ -215,34 +172,35 @@ public class MemberController {
 		Member updateMember = memberService.updateMember(memberDTO);
 		
 		if(updateMember != null) {
-			MemberDTO responseMemberDTO = memberDTO.builder()
-					.email(memberDTO.getEmail())
-					.name(updateMember.getName())
-					.pwd(updateMember.getPwd())
-					.job(updateMember.getJob())
-					.hasCareer(updateMember.getHasCareer())
-					.token(memberDTO.getToken())
-					.type(memberDTO.getType())
-					.fileAttached(updateMember.getFileAttached())
-					.build();
-			
+		MemberDTO responseMemberDTO = memberDTO.builder()
+				.email(memberDTO.getEmail()) // 이메일은 수정불가. 기존의 이메일
+				.name(updateMember.getName())
+				.pwd(updateMember.getPwd())
+				.job(updateMember.getJob())
+				.hasCareer(updateMember.getHasCareer())
+				.token(memberDTO.getToken()) // 토큰은 기존의 발급받은 토큰 사용
+				.type(memberDTO.getType())
+				.fileAttached(updateMember.getFileAttached())
+				.build();
+		
 			return ResponseEntity.ok().body(responseMemberDTO);
 		} else {
 			return ResponseEntity.badRequest().body("회원 수정 실패");
 		}
 	}
-
-	/*
-	// 로그아웃
-	@PostMapping("/logout")
-	public ResponseEntity<?> logout(@RequestBody MemberDTO memberDTO) {
-		Member member = memberService.getByCredentials(memberDTO.getEmail(), memberDTO.getPwd(), pwdEncoder);
-		if(member != null) {
-			// 로그아웃 처리 (별도의 로직 필요. 현재 로직으로 토큰 삭제 처리할 수 없음)
-			return ResponseEntity.ok().build();
-		} else {
-			return ResponseEntity.badRequest().body("회원 로그아웃 실패");
-		}
-	}
-	*/
+	
+//	// 로그아웃
+//	@PostMapping("/logout")
+//	public ResponseEntity<?> logout(@RequestBody MemberDTO memberDTO) {
+//		Member member = memberService.getByCredentials(memberDTO.getEmail(), memberDTO.getPwd(), pwdEncoder);
+//		System.out.println("-------------" + memberDTO.getEmail());
+//		System.out.println("-------------" + memberDTO.getPwd());
+//		System.out.println("-------------" + pwdEncoder);
+//		if(member != null) {
+//
+//			tokenProvider.deleteToken(memberDTO);
+//			return ResponseEntity.ok().build();
+//		}
+//		return ResponseEntity.badRequest().body("회원 로그아웃 실패");
+//	}
 }
--- backend/catchJob/src/main/java/com/project/catchJob/domain/member/GoogleOAuth.java
+++ backend/catchJob/src/main/java/com/project/catchJob/domain/member/GoogleOAuth.java
@@ -28,7 +28,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @PropertySource("classpath:application-oauth.properties")
 public class GoogleOAuth {
-
+/*
 	// https://velog.io/@hwsa1004/Spring-%EA%B5%AC%EA%B8%80-%EB%A1%9C%EA%B7%B8%EC%9D%B8-REST-API-%EA%B5%AC%ED%98%84-OAuth2
 	// https://developers.google.com/identity/protocols/oauth2/web-server?hl=ko 참고
 	
@@ -41,7 +41,7 @@ public class GoogleOAuth {
 	private final ObjectMapper objectMapper;
 	
 	private RestTemplate restTemplate;
-/*	
+	
 	@Value("${spring.security.oauth2.client.registration.google.clientId}")
 	private String googleClientId;
 	
@@ -66,12 +66,7 @@ public class GoogleOAuth {
 	    
 	    return oauthUrl;
 	}
-*/
-	@PostConstruct
-	public void init() {
-		this.restTemplate = new RestTemplate();
-	}
-	
+
 	// 일회용 코드를 다시 구글로 보내 엑세스 토큰을 포함한 json string이 담긴 responseEntity 받아옴
 	public ResponseEntity<String> requestAccessToken(String accessCode) {
 
@@ -79,16 +74,13 @@ public class GoogleOAuth {
 		Map<String, String> params = new HashMap<>();
 		
 		params.put("code", accessCode);
-	/*	params.put("client_id", googleClientId);
-		params.put("client_secret", googleClientSecret);
-		params.put("redirect_uri", googleRedirectUrl);*/
+		params.put("client-id", googleClientId);
+		params.put("client-secret", googleClientSecret);
+		params.put("redirect-uri", googleRedirectUrl);
 		params.put("grant_type", "authorization_code");
-		/*
+		
 		ResponseEntity<String> responseEntity = restTemplate.postForEntity(GOOGLE_TOKEN_URL, params, String.class);
 		// 스프링부트에서 다른 서버의 api 엔드포인트 호출할 때 restTemplate사용
-		*/
-		ResponseEntity<String> responseEntity = restTemplate.postForEntity(GOOGLE_TOKEN_URL, params, String.class);
-		
 		
 		if(responseEntity.getStatusCode() == HttpStatus.OK) {
 			return responseEntity;
@@ -101,17 +93,15 @@ public class GoogleOAuth {
 		System.out.println("response.getBody() = " + res.getBody());
 		GoogleOAuthTokenDTO googleOAuthTokenDTO = objectMapper.readValue(res.getBody(), GoogleOAuthTokenDTO.class);
 		return googleOAuthTokenDTO;
-		// GoogleOAuthTokenDTO : json형태를 자바 객체 형식으로 변경(역직렬화) 후 저장해서 담을 곳
+		// GoogleOAuthTokenDTO : json형태를 자바 객체 형식으로 변경 후 저장해서 담을 곳
 	}
 
-	// token받아와서 사용자 정보 요청
+	// get으로 token받아와서 사용자 정보 요청
 	public ResponseEntity<String> requestUserInfo(GoogleOAuthTokenDTO oAuthToken) {
 		
-		// header에 accessToken 담기
 		HttpHeaders headers = new HttpHeaders();
-		headers.add("Authorization", "Bearer " + oAuthToken.getAccess_token());
+		headers.add("Authorization", "Bearer" + oAuthToken.getAccess_token());
 		
-		// HttpEntity 하나 생성해 헤더를 담아서 restTemplate으로 구글과 통신하게 하기
 		HttpEntity<MultiValueMap<String, String>> req = new HttpEntity<>(headers);
 		ResponseEntity<String> res = restTemplate.exchange(GOOGLE_USERINFO_REQUEST_URL, HttpMethod.GET, req, String.class);
 		System.out.println("response.getBody() = " + res.getBody());
@@ -125,5 +115,5 @@ public class GoogleOAuth {
 		return googleUserInfoDTO;
 		// GoogleUserInfoDTO : json형태를 자바 객체 형식으로 변경 후 저장해서 담을 곳
 	}
-
+	*/
 }
