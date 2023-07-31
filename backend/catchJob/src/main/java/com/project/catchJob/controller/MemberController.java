efe77459769a4c41013aa109f59121124e0dfa33
uujjjjjnn <uujjjjjnn@gmail.com>
2023-07-31 09:57:21 +0900

    1

--- backend/catchJob/src/main/java/com/project/catchJob/controller/MemberController.java
+++ backend/catchJob/src/main/java/com/project/catchJob/controller/MemberController.java
@@ -20,7 +20,6 @@ import com.project.catchJob.service.MemberService;
 import com.project.catchJob.service.OAuthService;
 
 import lombok.extern.slf4j.Slf4j;
-import reactor.core.publisher.Mono;
 
 @Slf4j
 @RestController
